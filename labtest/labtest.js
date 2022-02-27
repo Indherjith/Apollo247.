7cb8c2002be7e3d54d97ee0e9ef6f3556a12088e
gaganrajput7 <gaganrajputbapora@gmail.com>
2022-02-27 14:57:10 +0530

    ok

--- labtest/labtest.js
+++ labtest/labtest.js
@@ -382,3 +382,49 @@ document.querySelector(".popular2").append(div);
     
     document.querySelector(".womenwell").append(div);
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    document.querySelector(".submitbtn").addEventListener("click", function() {
+        document.querySelector(".logincontainer").style.display = "flex";
+    })
+    document.querySelector(".next").addEventListener("click", function() {
+        document.querySelector(".logincontainer").style.display = "none";
+        document.querySelector(".otpcontainer").style.display = "flex";
+    
+    
+    })
+    document.querySelector(".next2").addEventListener("click", function() {
+        var password = document.querySelector(".otpinput").value;
+        if (password == "1234") {
+            alert("login successfull")
+            document.querySelector(".otpcontainer").style.display = "none";
+            window.location.href
+        } else {
+            alert("entered wrong otp try again")
+        }
+    })
+    document.querySelector("#Resend").addEventListener("click", function() {
+    
+        alert("otp Resended again");
+        document.querySelector(".otpcontainer").style.display = "flex";
+    
+    })
+    document.querySelector(".close2").addEventListener("click", function() {
+        document.querySelector(".otpcontainer").style.display = "none";
+    })
+    
+    
+    document.querySelector(".close").addEventListener("click", function() {
+        document.querySelector(".logincontainer").style.display = "none";
+    })
